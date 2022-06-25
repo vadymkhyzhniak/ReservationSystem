1a2b03db247513e10973d73ba856d0965f35ac2c
Vadym Khyzhniak <vadym.khyzhniak@check24.de>
2022-06-25 11:49:41 +0200

    Maps API added, Munich is marked and centered

--- src/client/resources/script.js
+++ src/client/resources/script.js
@@ -4,7 +4,7 @@ const munichCor = {lat: 48.137154, lng: 11.576124};
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 48.137154, lng: 11.576124},
-        zoom: 8,
+        zoom: 8
     });
 
     const marker = new google.maps.Marker({
