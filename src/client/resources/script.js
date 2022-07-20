9697f1889b1c0bc385f1ee0b00df3b2f9c91652e
Vadym Khyzhniak <vadym.khyzhniak@check24.de>
2022-07-20 18:41:40 +0200

    Restaurants dat files added

--- src/client/resources/script.js
+++ src/client/resources/script.js
@@ -1,15 +1,64 @@
 let map;
+
 const munichCor = {lat: 48.137154, lng: 11.576124};
 
-function initMap() {
+async function initMap() {
+    // fetch data and wait for response
+    const response = await fetch("./restaurants.json");
+    // ADD ERROR HANDLING HERE (e.g. if (!response.success) etc.)
+    // get data from response
+    const data = await response.json();
+    // get coordiantes from data
+    var array = [];
+    for (var i = 0; i < data.results.length; i++) {
+        if (!data.results[i].hasOwnProperty('price_level')) {
+            array[i] = [data.results[i].geometry.location.lat, data.results[i].geometry.location.lng, data.results[i].name, data.results[i].formatted_address, data.results[i].rating, "-"];
+        } else {
+            array[i] = [data.results[i].geometry.location.lat, data.results[i].geometry.location.lng, data.results[i].name, data.results[i].formatted_address, data.results[i].rating, data.results[i].price_level];
+        }
+    }
+    // create new google map
     map = new google.maps.Map(document.getElementById("map"), {
-        center: {lat: 48.137154, lng: 11.576124},
-        zoom: 8
+        center: munichCor,
+        zoom: 13
+    });
+    // create markers
+    for (var k = 0; k < array.length; k++) {
+        addMarker(array[k]);
+    }
+}
+
+function addMarker(props) {
+    const coordinates = {
+        lat: props[0],
+        lng: props[1]
+    };
+
+    var name = props[2];
+    const address = props[3];
+    const rating = props[4];
+    const price = props[5];
+
+    const icon = {
+        url: 'restaurant-icon.png', // url
+        scaledSize: new google.maps.Size(25, 40), // scaled size
+    };
+
+    var marker = new google.maps.Marker({
+        position: coordinates,
+        map: map,
+        icon: icon
     });
 
-    const marker = new google.maps.Marker({
-        position: munichCor,
-        map: map
+    var infoWindow = new google.maps.InfoWindow({maxWidth: 400});
+    google.maps.event.addListener(marker, 'click', function () {
+        var markerContent = "<h1 style='font-size: 55px'>" + name + "</h1>" +
+            "<h2 style='font-size: 40px'>" + address + "</h2>" +
+            "<img src='star.png' width='30' height='30' style='display: inline;'>" + "<h2 style='display: inline; font-size: 40px;'>" + " :" + rating + "</h2>" +
+            "<img src='dollarsign.png' width='30' height='30' style='display: inline; margin-left: 15px;'>" + "<h2 style='display: inline; font-size: 40px;'>" + " :" + price + "</h2>"
+        ;
+        infoWindow.setContent(markerContent);
+        infoWindow.open(map, marker)
     });
 }
 
