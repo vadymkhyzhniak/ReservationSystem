0b3af320f8fe8e5642011100062b18d7f89979aa
Vadym Khyzhniak <vadym.khyzhniak@check24.de>
2022-07-22 22:55:52 +0200

    JavaDoc

--- src/common/java/commonapplication/models/Reservation.java
+++ src/common/java/commonapplication/models/Reservation.java
@@ -7,6 +7,11 @@ import java.io.File;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+/**
+ * Class represents a reservation, used for processing the operations on reservations
+ *
+ * @author Chiheb Bacha
+ */
 public class Reservation implements Comparable<Reservation> {
 
     private String id;
@@ -22,6 +27,16 @@ public class Reservation implements Comparable<Reservation> {
 
     private boolean confirmed;
 
+    /**
+     * Creates a reservation
+     *
+     * @param reservationStart Time when the reservation starts
+     * @param reservationEnd Time when the reservation ends
+     * @param reservedBy User who reserved
+     * @param restaurant Actual restaurant
+     * @param table Table reserved
+     * @param reservationDate Reservation date
+     */
     public Reservation(LocalTime reservationStart, LocalTime reservationEnd, String reservedBy, Restaurant restaurant, Table table, LocalDate reservationDate) {
         this.reservationStart = reservationStart;
         this.reservationEnd = reservationEnd;
@@ -46,61 +61,128 @@ public class Reservation implements Comparable<Reservation> {
         Saver.addResId(resFile.getName().replace(".dat", "") + ",");
     }
 
+    /**
+     * Simple Reservation constructor
+     */
     // DONT DELETE
     public Reservation() {
     }
 
-
+    /**
+     * Gets the end time of the reservation
+     *
+     * @return LocalTime end time
+     */
     public LocalTime getReservationEnd() {
         return reservationEnd;
     }
 
+    /**
+     * Gets the start time of the reservation
+     *
+     * @return LocalTime start time
+     */
     public LocalTime getReservationStart() {
         return reservationStart;
     }
 
+    /**
+     * Gets the id of the reservation
+     *
+     * @return String id
+     */
     public String getId() {
         return id;
     }
 
-
+    /**
+     * Gets the user who made a reservation
+     *
+     * @return String user
+     */
     public String getReservedBy() {
         return reservedBy;
     }
 
-
+    /**
+     * Gets the actual restaurant, where the reservation was made
+     *
+     * @return Restaurant actual restaurant
+     */
     public Restaurant getRestaurant() {
         return restaurant;
     }
 
+    /**
+     * Gets the restaurant .dat file, storing the reservations' data
+     *
+     * @return File .dat file
+     */
     public File getResFile() {
         return resFile;
     }
 
+    /**
+     * Returns the restaurant information in form of String
+     *
+     * @return String information about restaurant
+     */
     public String toString() {
         return resInfo;
     }
 
+    /**
+     * Sets reservation status
+     *
+     * @param confirmed Status of the reservation
+     */
     public void setConfirmed(boolean confirmed) {
         this.confirmed = confirmed;
     }
 
+    /**
+     * Sets reservation status
+     *
+     * @return boolean Status of the reservation
+     */
     public boolean isConfirmed() {
         return confirmed;
     }
 
+    /**
+     * Gets the reservation date
+     *
+     * @return LocalDate reservation date
+     */
     public LocalDate getReservationDate() {
         return reservationDate;
     }
 
+    /**
+     * Gets the reserved table
+     *
+     * @return Table reserved table
+     */
     public Table getTable() {
         return table;
     }
 
+    /**
+     * Sets the table for the reservation
+     *
+     * @return Table table to be reserved
+     */
     public void setTable(Table table) {
         this.table = table;
     }
 
+    /**
+     * Checks if the time slot is available for the reservation
+     *
+     * @param reservationStart Reservation start time
+     * @param reservationEnd Reservation end time
+     * @return boolean availability of the time slot
+     */
     public boolean timeSuitable(LocalTime reservationStart, LocalTime reservationEnd) {
         boolean b1 = reservationStart.isAfter(this.reservationStart) && reservationStart.isBefore(this.reservationEnd);
         boolean b2 = reservationEnd.isAfter(this.reservationStart) && reservationEnd.isBefore(this.reservationEnd);
@@ -108,6 +190,12 @@ public class Reservation implements Comparable<Reservation> {
         return true;
     }
 
+    /**
+     * Compares if two reservations are equal
+     *
+     * @param reservation Reservation to compare with
+     * @return int result of the comparison
+     */
     @Override
     public int compareTo(Reservation reservation) {
         if (reservation == null)
