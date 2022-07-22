6be04ad460338abb95273a483e5dfbe1468eb910
Vadym Khyzhniak <vadym.khyzhniak@check24.de>
2022-07-22 23:01:52 +0200

    JavaDoc

--- src/common/java/commonapplication/models/Table.java
+++ src/common/java/commonapplication/models/Table.java
@@ -1,34 +1,66 @@
 package commonapplication.models;
 
+/**
+ * Class represents a table, used for handling operations on tables
+ *
+ * @author Chiheb Bacha
+ */
 public class Table {
     private long id;
     private Restaurant restaurant;
 
     private int available;
 
+    /**
+     * Gets the available table
+     *
+     * @return int Available table
+     */
     public int getAvailable() {
         return available;
     }
 
+    /**
+     * Sets the table available
+     *
+     * @param available Table to be set available
+     */
     public void setAvailable(int available) {
         this.available = available;
     }
 
+    /**
+     * Creates a table based on id and restaurant
+     *
+     * @param id ID of the table
+     * @param restaurant Restaurant, where the table is
+     */
     public Table(long id, Restaurant restaurant) {
         this.id = id;
         this.restaurant = restaurant;
     }
 
+    /**
+     * Simple table constructor
+     */
     // DONT DELETE
     public Table() {
     }
 
-
-
+    /**
+     * Gets the id of the table
+     *
+     * @return long ID of the table
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Returns the information about the table in form of String
+     *
+     * @return String information about table
+     */
     @Override
     public String toString() {
         return "<T:" + id + "><R:" + restaurant.getRestaurantFile().getName().replace(".dat", "") + "><A" + available + ">";
