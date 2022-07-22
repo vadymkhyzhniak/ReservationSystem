ca368169d5bb1f13761aa2246a91f821b261cc08
Vadym Khyzhniak <vadym.khyzhniak@check24.de>
2022-07-22 23:00:36 +0200

    JavaDoc

--- src/common/java/commonapplication/models/Speciality.java
+++ src/common/java/commonapplication/models/Speciality.java
@@ -1,5 +1,10 @@
 package commonapplication.models;
 
+/**
+ * Enum represents cuisine types of the restaurants
+ *
+ * @author Chiheb Bacha
+ */
 public enum Speciality {
     Unbekannt, Chinesisch, Deutsch, Griechisch, Hamburger, Indisch,
     Italienisch, Japanisch, Döner, Mexikanisch, Pizza,
